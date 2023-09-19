629a78ec86db99be8c1aa973beb85a08f2d70a05
RH603 <gksgh123@naver.com>
2023-09-19 11:19:47 +0900

    array

---
+++ 23.9.19/비교문장/array.js
@@ -0,0 +1,16 @@
+// console.log('array')
+const pokemon = ['피카츄','이브이','라이츄','파이리','꼬부기']
+// push는 배열 마지막에 요소를 추가
+// 배열의 마지막 요소를 제거
+pokemon.pop()
+pokemon.push('망나뇽')
+// 포켓몬 배열에 이상해씨를 추가해주세여
+pokemon.push('이상해씨','이상해풀','이상해꽃')
+console.log(pokemon)
+// 배열의 길이를 알아보는 방법
+console.log(pokemon.length)
+// 배열의 요소를 셀때는 0부터 시작한다
+console.log(pokemon[0])
+console.log(pokemon[3])
+console.log(pokemon[5])
+console.log(pokemon[6])
