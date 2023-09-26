edfb004ec1681190832dc476a72c59fc346adb6e
RH603 <gksgh123@naver.com>
2023-09-26 12:33:46 +0900

    forEach 추가

--- 23.9.26/counter/script.js
+++ 23.9.26/counter/script.js
@@ -6,21 +6,33 @@ const dataList = document.querySelectorAll('[data-counter]')
 // console.log(dataList)
 // console.log(dataList[0].dataset.counter)
 
-for (let i = 0; i < dataList.length; i++) {
-    const target = dataList[i].dataset.counter
-    const timerText = dataList[i].querySelector('.timer')
-    const step = Math.floor(target / 400)
-    console.log(step)
+// for (let i = 0; i < dataList.length; i++) {
+//     const target = dataList[i].dataset.counter
+//     const timerText = dataList[i].querySelector('.timer')
+//     const step = Math.floor(target / 400)
+//     console.log(step)
+//     let index = 0;
+//     console.log(target)
+//     console.log(index)
+//     console.log(timerText)
+//     setInterval(() => {
+//         if (target > index) {
+//             index = index + step
+//             console.log(index)
+//             timerText.textContent = index
+//         }
+//     }, 1)
+// }
+
+dataList.forEach(item => {
     let index = 0;
-    // console.log(target)
-    // console.log(index)
-    // console.log(timerText)
-    setInterval(() => {
-        //증가
-        if (target >index) {
+    const target = item.dataset.counter
+    const timerText = item.querySelector('.timer')
+    const step = Math.floor(target / 200)
+    setInterval(()=>{
+        if(index < target){
             index = index + step
-            // console.log(index)
             timerText.textContent = index
         }
-    }, 1)
-}
+    })
+})
