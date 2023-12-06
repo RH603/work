069d425be17bc11749174e0f4177b58fd45c5fb5
RH603 <gksgh123@naver.com>
2023-12-06 17:32:15 +0900

    특강

--- 23.11.30/slide.js
+++ 23.11.30/slide.js
@@ -1,4 +1,6 @@
 const slider = document.querySelector(".gallery");
+const nextBtn = document.querySelector(".swiper-button-next")
+const prevBtn = document.querySelector(".swiper-button-prev")
 let startX = 0;
 let scrollLeft = 0;
 const SCROLL_SPEED = 3;
@@ -25,6 +27,16 @@ slider.addEventListener("mouseup",()=>{
 slider.addEventListener("mouseleave",()=>{
     slider.removeEventListener("mousemove",doScroll)
     slider.classList.remove("onDrag")
+})
+nextBtn.addEventListener("click",()=>{
+    console.log(slider.scrollLeft)
+    slider.scrollLeft = slider.scrollLeft + 697
+    // alert("click")
+})
+prevBtn.addEventListener("click",()=>{
+    console.log(slider.scrollLeft)
+    slider.scrollLeft = slider.scrollLeft - 697
+    // alert("click")
 })
 // slider.addEventListener("mousemove",()=>{
 //     console.log("mousemove")
