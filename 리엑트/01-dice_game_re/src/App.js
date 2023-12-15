4dae3ade57932358c32827229b52661f9de8f392
RH603 <gksgh123@naver.com>
2023-12-15 17:31:08 +0900

    리엑트

---
+++ 리엑트/01-dice_game_re/src/App.js
@@ -0,0 +1,25 @@
+// import './App.css';
+import logo from "./assets/logo.png"
+import Button from "./Button";
+
+
+function App() {
+  return (
+      <div className="App">
+      <div>
+        <img src={logo} className="App-logo" />
+        <h1 className="App-title">주사위게임</h1>
+        <div>
+          <Button className="App-button blue" onClick={handleRollClick}>던지기</Button>
+          {/* <Button className="App-button red" onClick={handleClearClick}>처음부터</Button> */}
+        </div>
+      </div>
+      <div className="App-boards">
+        {/* <Board name="나" color="blue" gameHistory={gameHistory} /> */}
+        {/* <Board name="상대" color="red" gameHistory={otherGameHistory} /> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
